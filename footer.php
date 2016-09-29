9191c6998bc7a47ac7a5e4aa1e699f5bd0f6c1c9
Ye Ding <guiewy@gmail.com>
2016-09-29 22:49:54 +0800

    v0.9.29

--- footer.php
+++ footer.php
@@ -1,1 +1,1 @@
-<small class="text-muted pull-right">&copy; 2016-2017 <a href='http://deepera.com' target="_blank">Deepera Co., Ltd.</a></small>
+<small class="text-muted pull-right">&copy; 2016 <a href='http://deepera.com' target="_blank">Deepera Co., Ltd.</a></small>
